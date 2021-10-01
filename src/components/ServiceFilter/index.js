0bf658bc20a9c02c191f982c4bbd031e723aa670
lorenapassagem <lorenapassagem@gmail.com>
2021-10-01 18:16:49 -0300

    create components service filter

---
+++ src/components/ServiceFilter/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ServiceFilter = styled.nav`
+    background-color: rgb(61,76,92);
+`;
+
+export const Label = styled.p`
+    color: #fffcee;
+    font-family: 'comfortaa';
+    font-size: 20px;
+`;
