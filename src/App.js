1a2cdad0f9b46724744d9ca99464f59ae3e1059d
lorenapassagem <lorenapassagem@gmail.com>
2021-10-01 19:01:36 -0300

    :lipstick: change style from service filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import './App.css';
+
 import IconSearch from './components/IconSearch';
 import LogoMain from './components/LogoMain';
 import HeaderMain from './components/HeaderMain';
--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
+import IconFilter from '../IconFilter';
 
 export const ServiceFilterStyle = styled.nav`
     background-color: rgb(61,76,92);
+    display: flex;
+
+    & > ${IconFilter}{
+        margin-right: 20px;
+    }
 `;
 
 export const Label = styled.p`
