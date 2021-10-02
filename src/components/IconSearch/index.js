2a7b0c9a1b6af1b9bbe2908e8b501a9f0779df7b
lorenapassagem <lorenapassagem@gmail.com>
2021-10-02 17:31:14 -0300

    remove background-color from icon search

--- src/components/IconSearch/index.js
+++ src/components/IconSearch/index.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import Search from '../../imagens/search.svg';
 
-const IconSearch = styled.img.attrs({ src: Search, alt: 'logo de uam lupa' })`
- background-color:red ;
-`;
+const IconSearch = styled.img.attrs({ src: Search, alt: 'logo de uam lupa' })``;
 
 export default IconSearch;
