4838b51859d4f409ae3dd427ce1b4434d843de4d
lorenapassagem <lorenapassagem@gmail.com>
2021-10-04 11:26:12 -0300

    add styles in service filter

--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -4,6 +4,8 @@ import IconFilter from '../IconFilter';
 export const ServiceFilterStyle = styled.nav`
     background-color: rgb(61,76,92);
     display: flex;
+    align-items: center;
+    padding: 8px 16px;
 
     & > ${IconFilter}{
         margin-right: 20px;
@@ -14,4 +16,5 @@ export const Label = styled.p`
     color: #fffcee;
     font-family: 'comfortaa';
     font-size: 20px;
+    line-height: 1em;
 `;
